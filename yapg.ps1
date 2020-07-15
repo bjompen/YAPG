60ec754b86d0da42f441db542f113fedfc64e4be
Björn Sundling <bjorn.sundling@advania.com>
2020-07-15 13:47:29 +0200

    generate more than one password

--- yapg.ps1
+++ yapg.ps1
@@ -13,35 +13,40 @@ function New-YapgPassword {
         [switch]$Leet,
 
         [Parameter()]
-        [string]$Dictionary = "$PSScriptRoot\en-GB.dic"
+        [string]$Dictionary = "$PSScriptRoot\en-GB.dic",
+
+        [Parameter()]
+        [int]$Passwords = 1
     )
 
     $AllWords = ([System.IO.StreamReader]::new($Dictionary).ReadToEnd()).Split("`n").Where({$_ -match '^[a-zA-Z]'})
 
-    [array]$Words = 1..$WordCount | foreach {
-        $CurrWord = ($AllWords | Get-Random).split('/')[0]
-        If ($Capitalize) {
-            $CurrWord = $CurrWord.ToCharArray()
-            $i = Get-Random -Minimum 0 -Maximum $CurrWord.Count
-            $CurrWord[$i] = $CurrWord[$i].ToString().ToUpper()
-            $CurrWord = -join $CurrWord
+    1.. $Passwords | ForEach-Object { 
+        [array]$Words = 1..$WordCount | ForEach-Object {
+            $CurrWord = ($AllWords | Get-Random).split('/')[0]
+            If ($Capitalize) {
+                $CurrWord = $CurrWord.ToCharArray()
+                $i = Get-Random -Minimum 0 -Maximum $CurrWord.Count
+                $CurrWord[$i] = $CurrWord[$i].ToString().ToUpper()
+                $CurrWord = -join $CurrWord
+            }
+            $CurrWord
         }
-        $CurrWord
-    }
 
-    if ($AddChars) {
-        function GenerateChar {
-            [char][int](Get-Random -Minimum 33 -Maximum 48)
+        if ($AddChars) {
+            function GenerateChar {
+                [char][int](Get-Random -Minimum 33 -Maximum 48)
+            }
+            0..($Words.Count - 2) | ForEach-Object {
+                $Words[$_] = "$($Words[$_])$(GenerateChar)" 
+            }
         }
-        0..($Words.Count - 2) | foreach {
-            $Words[$_] = "$($Words[$_])$(GenerateChar)" 
+        
+        if ($Leet) {
+            -join ($Words -creplace 'l','1' -creplace 'e','3' -creplace 't','7' -creplace 'o','0')
         }
-    }
-    
-    if ($Leet) {
-        -join ($Words -creplace 'l','1' -creplace 'e','3' -creplace 't','7' -creplace 'o','0')
-    }
-    else { 
-        -join $Words
+        else { 
+            -join $Words
+        }
     }
 }
